35193374f98d9a2c592419d326302207a53f3b01
Kamal Kadayat <kmlkadayat@gmail.com>
2024-04-10 12:25:01 +0545

    first commit

---
+++ src/components/Sidebar/Help.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Help = () => {
+    return (
+        <div className="help-container">
+            {/* Help content */}
+        </div>
+    );
+};
+
+export default Help;
